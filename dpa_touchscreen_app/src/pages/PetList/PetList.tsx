095210eb82251c9dc40bc5d1983f3ae3a425ed1b
Katie Patterson <kpatterson67@gmail.com>
2022-09-29 23:23:38 -0500

    refetching

--- dpa_touchscreen_app/src/pages/PetList/PetList.tsx
+++ dpa_touchscreen_app/src/pages/PetList/PetList.tsx
@@ -4,11 +4,15 @@ import { FC } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 export const PetList: FC = () => {
-  const { isLoading, error, data, isFetching } = useQuery(["petList"], () =>
+  const { isLoading, error, data, isFetching } = useQuery(["petList"], (() =>
     axios
       .get("http://localhost:3001/petlist")
       .then((res) => res.data)
-  );
+  ), {
+    /* refetch every 10 minutes */
+    refetchInterval: 1000 * 60 * 10,
+    staleTime: 1000 * 60 * 10,
+  });
 
   if (isLoading) return (
     <div>Loading...</div>
@@ -21,9 +25,16 @@ export const PetList: FC = () => {
     );
   };
 
+  const petList = data?.pets?.map((pet: any) => {
+    return (
+      <div>{pet.name}</div>
+    );
+  });
+
   return (
     <div>
       <header>pet list</header>
+      <p>{petList}</p>
     </div>
   );
 };
