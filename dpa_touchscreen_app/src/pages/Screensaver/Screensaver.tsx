52956deeb280d95aada2b1b9e2d8122d5fabf13e
Katie Patterson <kpatterson67@gmail.com>
2023-01-31 23:46:41 -0600

    scroll the image

--- dpa_touchscreen_app/src/pages/Screensaver/Screensaver.tsx
+++ dpa_touchscreen_app/src/pages/Screensaver/Screensaver.tsx
@@ -20,6 +20,7 @@ export const Screensaver: FC = () => {
     <div className="screensaver">
       <div className="screensaver-animation">
         <img src={animation} />
+        <img className="img2" src={animation} />
       </div>
       <div className="screensaver-cta">
         <span className="cta-text">FIND YOUR BEST FRIEND</span>
